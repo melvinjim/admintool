4702f6269e658e7407c10f9d46f9668eaf1a08a7
melvinjim <melvinjimenezang@hotmail.com>
2022-06-22 10:25:50 -0500

    correcion in creation DB

--- actions/admintool.go
+++ actions/admintool.go
@@ -1,41 +1,11 @@
 package actions
 
 import (
-	"admintool/models"
 	"net/http"
 
 	"github.com/gobuffalo/buffalo"
-	"github.com/gobuffalo/pop/v6"
 )
 
-func CreateEmployees(c buffalo.Context) error {
-
-	createEmployee := models.Employee{
-		Name: "Daniel",
-	}
-
-	tx := c.Value("tx").(*pop.Connection)
-
-	err := tx.Create(&createEmployee)
-	if err != nil {
-		return err
-	}
-
-	return c.Redirect(http.StatusSeeOther, "/")
-}
-
 func Admintool(c buffalo.Context) error {
-
-	employees := []models.Employee{}
-
-	tx := c.Value("tx").(*pop.Connection)
-
-	err := tx.All(&employees)
-	if err != nil {
-		return err
-	}
-
-	c.Set("employees", employees)
-
 	return c.Render(http.StatusOK, r.HTML("admintool/index.plush.html"))
 }
