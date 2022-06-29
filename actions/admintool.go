598af985da99c29b06344bf7ee6a85910340ebcf
melvinjim <melvinjimenezang@hotmail.com>
2022-06-29 11:05:59 -0500

    corrections and validations in the form new user

--- actions/admintool.go
+++ actions/admintool.go
@@ -2,32 +2,11 @@ package actions
 
 import (
 	"admintool/models"
-	"log"
 	"net/http"
-
-	v "github.com/gobuffalo/validate"
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop/v6"
 )
-
-func main() {
-	u := models.Employee{Name: "", Email: "", WorkTelephone: ""}
-	errors := v.Validate(&u)
-	log.Println(errors.Errors)
-}
-
-func (u *models.Employee) IsValid(errors *v.Errors) {
-	if u.Name == "" {
-		errors.Add("name", "Name must not be blank!")
-	}
-	if u.Email == "" {
-		errors.Add("email", "Email must not be blank!")
-	}
-	if u.WorkTelephone == "" {
-		errors.Add("work telephone", "work telephone must not be blank!")
-	}
-}
 
 func Admintool(c buffalo.Context) error {
 	employees := []models.Employee{}
@@ -45,20 +24,34 @@ func Admintool(c buffalo.Context) error {
 }
 
 func AddUser(c buffalo.Context) error {
+	employee := models.Employee{}
+	c.Set("employee", employee)
+
 	return c.Render(http.StatusOK, r.HTML("users/new.plush.html"))
 }
 
 func ReceiveData(c buffalo.Context) error {
-	u := models.Employee{}
-	if err := c.Bind(&u); err != nil {
+	employee := models.Employee{}
+	if err := c.Bind(&employee); err != nil {
 		return err
 	}
 
 	tx := c.Value("tx").(*pop.Connection)
 
-	err := tx.Create(&u)
+	verrs, err := employee.Validate(tx)
 	if err != nil {
 		return err
+	}
+
+	if verrs.HasAny() {
+		c.Set("employee", employee)
+		c.Set("errors", verrs)
+		return c.Render(http.StatusOK, r.HTML("users/new.plush.html"))
+	}
+
+	errCreate := tx.Create(&employee)
+	if errCreate != nil {
+		return errCreate
 	}
 
 	return c.Redirect(http.StatusSeeOther, "/")
