a6fd4d204778ecb8f1b0e7761e1976d957709777
melvinjim <melvinjimenezang@hotmail.com>
2022-07-06 18:02:01 -0500

    add a user in a new table

--- models/employer.go
+++ models/employer.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Employer struct {
-	ID        uuid.UUID `json:"id" db:"id"`
-	Name      string    `json:"name" db:"name"`
-	Address   string    `json:"address" db:"address"`
-	Phone     string    `json:"phone" db:"phone"`
-	CreateAt  time.Time `json:"create_at" db:"create_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID        uuid.UUID `form:"id" db:"id"`
+	Name      string    `form:"name" db:"name"`
+	Address   string    `form:"address" db:"address"`
+	Phone     string    `form:"phone" db:"phone"`
+	CreateAt  time.Time `form:"created_at" db:"created_at"`
+	UpdatedAt time.Time `form:"updated_at" db:"updated_at"`
 }
